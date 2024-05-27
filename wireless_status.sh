54141f53597460d6a76cfc8d57bde3634a6b2730
Nicholas Riley <com-github@sabi.net>
2024-05-27 12:40:59 -0400

    Include the shell script.

---
+++ wireless_status.sh
@@ -0,0 +1,8 @@
+#!/bin/zsh -ef
+
+# <bitbar.title>Wireless Status</bitbar.title>
+# <bitbar.author>Nicholas Riley/bitbar.author>
+# <bitbar.author.github>nriley</bitbar.author.github>
+# <bitbar.desc>Displays the current Wi-Fi status, including channel and signal strength</bitbar.desc>
+
+/usr/local/bin/wireless_status
